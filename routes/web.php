1fceb45f273fc174d253dea7a71dcb124d02ddf4
mabdulgani <61041849+mabdulgani@users.noreply.github.com>
2020-03-12 19:21:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,22 @@ Route::get('/dokter/edit/{id}','DokterController@edit');
 Route::post('/dokter/update','DokterController@update');
 Route::get('/dokter/hapus/{id}','DokterController@hapus');
 Route::get('/dokter/cari','DokterController@cari');
+// Route Crud Pasien
+Route::get('/pasien','PasienController@index');
+Route::get('/pasien/tambah','PasienController@tambah');
+Route::post('/pasien/store','PasienController@store');
+Route::get('/pasien/edit/{id}','PasienController@edit');
+Route::post('/pasien/update','PasienController@update');
+Route::get('/pasien/hapus/{id}','PasienController@hapus');
+Route::get('/pasien/cari','PasienController@cari');
+// Route Crud Registrasi
+Route::get('/poli','PoliController@index');
+Route::get('/poli/tambah','PoliController@tambah');
+Route::post('/poli/store','PoliController@store');
+Route::get('/poli/edit/{id}','PoliController@edit');
+Route::post('/poli/update','PoliController@update');
+Route::get('/poli/hapus/{id}','PoliController@hapus');
+Route::get('/poli/cari','PoliController@cari');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
